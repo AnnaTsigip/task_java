41e38b269d67f5434267cb96f4664800b145058b
Anna <anna_tsigip@mail.ru>
2022-06-18 19:09:20 +0300

    name\age

---
+++ Program_002.java
@@ -0,0 +1,13 @@
+public class Program_002{      
+    public static void main (String args[]){
+          
+        display("Tom", 34);
+        display("Bob", 28);
+        display("Sam", 23);
+    }
+    static void display(String name, int age){
+         
+        System.out.println(name);
+        System.out.println(age);
+    }
+}
