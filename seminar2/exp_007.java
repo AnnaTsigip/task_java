7fdbc3b09ae5bc10415b9663f49d1961a989a297
Anna <anna_tsigip@mail.ru>
2022-06-28 22:07:44 +0300

    all

---
+++ seminar2/exp_007.java
@@ -0,0 +1,11 @@
+package seminar2;
+
+public class exp_007 {
+
+    long[] source = new long[1];
+    long[] copy = new long[source.length + 1];
+    System.arraycopy(source, 0, copy, 0, source.length);
+    source = copy;
+    
+
+}
